312409ae339a9b69a56566c2e21d8cf6a766fe37
Anil khand <anilkhandthakuri@gmail.com>
2024-12-01 23:50:00 +1100

    Updated isadmin and auth with joi validator for create and update book

--- src/models/books/bookSchema.js
+++ src/models/books/bookSchema.js
@@ -1,47 +1,47 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const BookSchema = new mongoose.Schema(
-//   {
-//     status: {
-//       type: String,
-//       default: "inactive",
-//     },
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     author: {
-//       type: String,
-//       required: true,
-//     },
+const BookSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      default: "inactive",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
 
-//     isbn: {
-//       type: String,
-//       unique: true,
-//       index: 1,
-//       required: true,
-//     },
-//     imageURL: {
-//       type: String,
-//       required: true,
-//     },
-//     availability: {
-//       type: Boolean,
-//       default: "",
-//     },
-//     summary: {
-//       type: String,
-//       required: true,
-//     },
+    isbn: {
+      type: String,
+      unique: true,
+      index: 1,
+      required: true,
+    },
+    imageURL: {
+      type: String,
+      required: true,
+    },
+    availability: {
+      type: Boolean,
+      default: "",
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
 
-//     publishedYear: {
-//       type: Number,
-//       required: true,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+    publishedYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// export default mongoose.model("books", BookSchema);
+export default mongoose.model("books", BookSchema);
